4b74b1a925ae5dd6644fbaef7c313db550431731
seba <sansoni.sebas@gmail.com>
2021-05-09 10:37:03 -0300

    Se crearon las clases mapa y se acomodarons sus parametros. Falta incorporarlo al bucle principal

--- scripts/ICM_SLAM.py
+++ scripts/ICM_SLAM.py
@@ -104,7 +104,8 @@ def actualizar_mapa(mapa,yy,obs,Lact,config):
      - yy: Es el mismo mapa anterior solo que contiene los árboles observados.
      - [(x,y) x Nobs] obs: Lista de observaciones en coordenadas cartecianas.
        Nobs son la cantidad de observaciones filtradas (sin outliers). 
-     - [int] Lact: cantidad de árboles hasta el momento
+     - [int] Lact: cantidad de árboles hasta el momento (Landmarks
+       activos/actuales)
      - config: parámetros de configuración dado por el método ConfigICM
     Salidas:
      - [2 x Lact] Mapa: Es una matriz con las posiciones 2D de 'Lact' árboles.
@@ -162,10 +163,10 @@ def filtrar_z(z,config):
      - config: parámetros de configuración
 
     Salidas:
-     - float [L_vistos x 4] zz: Salida filtrada , $1 revisar!!   [dist ang x y]
+     - numpy array [dist ang x y] zz: Salida filtrada y reformateada, $1 revisar!!   
     """
     z=medfilt(z)  #filtro de mediana con ventana 3 para borrar observaciones laser outliers
-    zz=copy(z)#+0.0  # $1 copia para no sobreescribir
+    zz=copy(z) #copia para no sobreescribir
     #hallo direcciones con observacion, el [0] es para solo quedarte con el
     #array, no con la tupla
     nind=np.where(z<config.rango_laser_max)[0] 
@@ -187,22 +188,6 @@ def filtrar_z(z,config):
 
     return zz
 
-def tras_rot_z(x,z):
-    """
-     - Rota y traslada las observaciones de acuerdo a la pose actual
-     - Transforma del body frame al global frame
-
-    :math:`x_t=[ x_{t,x}, x_{t,y}, x_{t,\theta}]^T` 
-    :math:`z_i=\{z_{t,i}:i=1,\cdots,n_t\}` :math:`z_{t,i}=[ z_{t,i,d}, z_{t,i,\theta} ]^T`
-
-    """
-
-    x=x.reshape((3,1))
-    ct=np.cos(x[2]-np.pi/2.0)[0]
-    st=np.sin(x[2]-np.pi/2.0)[0]
-    R=np.array([[ct,st],[-st,ct]])
-    z[:,2:4]=np.matmul(z[:,2:4],R)+np.matlib.repmat(x[0:2].T,z.shape[0],1)
-    return z
 
 class ICM_method:
     """
@@ -247,7 +232,12 @@ class ICM_method:
 
     def fun_xn(self,x):
         """
-        Función a minimizar
+        Función a minimizar. 
+
+        Parte del argumento de la Ec. (14) del papaer. Esta función incluye a
+        fun_x, ya que usa la información previa (fun_x) e información futura.
+        En este codigo solo se incorpora la información futura. 
+
         Entrada:
          - x: Pose
 
@@ -335,7 +325,6 @@ class ICM_method:
         Salida:
          - f: potencial energético.
 
-        Escribir la operación que se realiza (que ec del paper?)$1
         """
 
         z=self.zopt
@@ -364,11 +353,245 @@ class ICM_method:
            self.config.cte_odom*np.matmul(ooo.T,ooo)
         return f
 
+    def itererar(self):
+        """
+        Este método refina mediante ICM el mapa y las poses históricas del
+        vehículo.
+
+        Argumentos de entrada:
+        ----------------------
+        Variables:
+         - Posiciones actuales 'x'
+         - Mapa actual 'yy'
+        Constantes:
+         - 'config'
+         - Mediciones 'z'
+         - Cantidad de landmarks vistos 'Lact'
+         - acciones de control 'u'
+        
+        Argumentos de salida:
+        ---------------------
+         - Mapa actualizado 'yy'
+         - Posiciones refinadas 'x' 
+        """
+
+        xt=copy(x0) #pose inicial
+        y=np.zeros((2,config.L)) #guarda la posicion de los a lo sumo L arboles del entorno
+        cant_obs_i=np.zeros(config.L)  #guarda la cantidad de veces que se observó el i-ésimo árbol
+        z=filtrar_z(zz[:,0],config)  #filtro la primer observacion [dist ang x y] x #obs
+        if z.shape[0]==0:
+            return
+            #continue #si no hay observaciones pasar al siguiente periodo de muestreo
+        
+        zt=tras_rot_z(xt,z) #rota y traslada las observaciones de acuerdo a la pose actual
+        y,cant_obs_i,c,Lact=actualizar_mapa(y,yy,zt[:,2:4],Lact,config)  #actualizo (o creo) ubicación de arboles observados
+    
+        #BUCLE TEMPORAL
+        for t in range(1,config.Tf):
+            z=filtrar_z(zz[:,t],config)  #filtro observaciones no informativas del tiempo t: [dist ang x y] x #obs
+            if z.shape[0]==0:
+                xt=(xt.reshape(3)+x[:,t+1])/2.0
+                x[:,t]=xt
+                continue #si no hay observaciones pasar al siguiente periodo de muestreo
+            
+            zt=tras_rot_z(x[:,t],z)  #rota y traslada las observaciones de acuerdo a la pose actual
+            y,cant_obs_i,c,Lact=actualizar_mapa(y,yy,zt[:,2:4],Lact,config)  #actualizo (o creo) ubicación de arboles observados
+            if t+1<config.Tf:
+                xt=ICM.minimizar_xn(z[:,0:2],y[:,c].T,xt,x[:,t+1],u[:,t-1:t+1],odometria[:,t-1:t+2])
+            else:
+                xt=ICM.minimizar_x(z[:,0:2],y[:,c].T,xt,u[:,t-1],odometria[:,t-1:t+1])
+            x[:,t]=xt
+        
+        #filtro ubicaciones estimadas
+        [yy,cant_obs_i,Lact]=filtrar_y(y,cant_obs_i,Lact,config)
+        yy=yy[:,:Lact]
+
+
+class Mapa:
+    """
+    Esta clase define los metodos utilizados para mapear y refinar un mapa.
+    """
+
+    def __init__(self,config):
+        # Parámetros de configuracion inicial
+        self.L=config.L
+        self.cota=config.cota
+        self.dist_thr=config.dis_thr
+        self.clear() 
+
+        self.Landmarks_actuales=0
+        
+        #self.mapa=np.zeros((2,config.L)) #guarda la posicion de los a lo sumo L arboles del entorno
+    def clear_obs(self):
+        """
+        Inicialización de variables internas
+        """
+        self.cant_obs_i=np.zeros(self.L)  #guarda la cantidad de veces que se observó el i-ésimo árbol
+
+    def actualizar(self,mapa,mapa_estatico,obs):
+        """
+
+        actualizar(self,mapa,mapa_referencia,obs,Lact)
+        
+        mapa,cant_obs_i,c,Lact=actualizar_mapa(mapa,yy,obs,Lact)
+        
+        Actualiza las variables relacionadas con la construcción del mapa. Como
+        argumentos de entrada son el mismo mapa y las observaciones (de una sola
+        medicion? o pueden ser varias observaciones?). 
+        Ver una forma de hacer una clase "Mapa" para hacer estas actualizaciones,
+        ya que hay mucha información redundante. 
+    
+        Mapa es la estimación de los árboles que se tiene hasta el momento dentro de la iteracion ICM.
+        **yy** es la estimación de los árboles que se usará para realizar el etiquetado de las obs nuevas. 
+        En la iteracion 0 yy=mapa, pero en las iteraciones siguientes yy es el mapa final estimado en la iteración ICM
+        anterior.
+        
+        #actualizo (o creo) ubicación de arboles observados
+    
+        Parámetros:
+        -----------
+    
+        Entradas:
+         - [2 x Lact] Mapa: Es una matriz con las posiciones 2D de todos árboles.
+         - yy o mapa_estatico: Es el mapa de referencia, que NO se modifca.
+         - [(x,y) x Nobs] obs: Lista de observaciones en coordenadas cartecianas.
+           Nobs son la cantidad de observaciones filtradas (sin outliers). 
+         - [int] Lact: cantidad de árboles hasta el momento (Landmarks
+           activos/actuales)
+        Salidas:
+        'Externas'
+         - [2 x Lact] Mapa: Es una matriz con las posiciones 2D de 'Lact' árboles.
+         - vector de etiquetas 'c': Vector de etiquetas de cada landmark. Dice a que landmark
+           corresponde cada medición.
+         
+        'Internas'
+         - int [1 x Lact] cant_obs_i: Conteo de la cantidad de veces que se observo
+           un árbol.
+         - [int] Lact: cantidad de árboles actualizado.
+        """
+        Lact=self.Landmarks_actuales
+        cant_obs_i=self.cant_obs_i
+
+        if Lact==0:#este bucle es solamente para t=0 de la iteración ICM 0
+            c=fcluster(linkage(pdist(obs)),self.dist_thr)-1  #calculo clusters iniciales
+            Lact=np.max(c)+1  #cantidad de arboles iniciales
+            for i in range(Lact):
+                mapa[:,i]=np.mean(obs[c==i,:],axis=0).T #calculo el centro de cada cluster
+                cant_obs_i[i]=len(c[c==i])
+    
+        else:
+            #me fijo si las observaciones corresponden a un arbol existente
+            distancias=cdist(mapa_estatico[:,:Lact].T,obs)#matriz de distancias entre yy y las obs nuevas
+            min_dist=np.amin(distancias,axis=0)#distancia minima desde cada obs a un arbol de yy
+            c=np.argmin(distancias,axis=0)#etiqueta del arbol de yy que minimiza la distancia a cada obs nueva
+            c[min_dist>self.dist_thr]=-1#si esta lejos de los arboles de yy le asigno la etiqueta -1 momentaneamente
+            #armo cluster con observaciones de arboles nuevos
+            ztt=obs[min_dist>self.dist_thr,:]#extraigo las obs nuevas que estan lejos de los árboles de yy
+            if ztt.shape[0]>1:#si hay mas de una observacion de un arbol no mapeado aun
+                cc=Lact+fcluster(linkage(pdist(ztt[:,2:4])),self.dist_thr)-1 #calculo clusters y le coloco una etiqueta nueva (a partir de Lact=max etiqueta+1)
+                c[c==-1]=cc#a todos los árboles con etiqueta -1 le asigno su nueva etiqueta
+    
+            elif ztt.shape[0]==1:#si hay sólo una observacion de un arbol no mapeado aun
+                c[c==-1]=Lact
+    
+            Lact=np.amax(np.append(c+1,Lact))#actualizo la cantidad de arboles mapeados hasta el momento
+            #actualizo (o creo) ubicación de arboles observados
+            for i in range(Lact):
+                if len(c[c==i])>0:
+                    mapa[:,i]=np.sum(zt[c==i,2:4],axis=0)/(cant_obs_i[i]+len(c[c==i]))\
+                            +mapa[:,i]*cant_obs_i[i]/(cant_obs_i[i]\
+                            +len(c[c==i]))
+    
+                    cant_obs_i[i]=cant_obs_i[i]+len(c[c==i])
+
+        self.Landmarks_actuales=Lact
+        self.cant_obs_i=cant_obs_i
+    
+        return mapa,c
+
+
+    def filtrar(self,mapa):
+       """
+       [y,cant_obs_i,Lact]=filtrar_y(y,cant_obs_i)
+       
+       Se filtra el mapa, eliminando landmarks poco observados y unificando
+       landmarks cercanos. 
+       
+       Parámetros
+       ----------
+    
+       Entradas:
+       'Externas'
+        - y o mapa: Mapa de entrada
+       
+       'Internas'
+        - cant_obs_i: Cantidad de veces que se observo cada árbol ordenados por su
+          índice
+    
+       Salidas:
+       'Externas'
+        - yy o mapa_filtrado: mapa filtrado con los árboles más observados
+       'Internas'
+        - cant_obs_i: Cantidad de observaciones luego de filtrar 
+       """
+       Lact=self.Landmarks_actuales 
+       cant_obs_i=self.cant_obs_i
+
+       cant_obs_i=cant_obs_i[0:Lact] #saco ceros innecesarios
+       ind=np.where(cant_obs_i<self.cota)[0]  #indices de arboles poco observados
+       if ind.size>0:  #si hay arboles poco observados
+           Lact=Lact-ind.size  #reduzco la cantidad de arboles observados hasta el momento
+           ind2=np.where(cant_obs_i>=self.cota)[0] #indices de arboles observados muchas veces
+           mapa=mapa[:,ind2] #elimino las posiciones estimadas de los arboles vistos pocas veces
+           np.concatenate((mapa,np.zeros((2,len(ind)))),axis=1)  #le devuelvo a y su dimension original completando con ceros
+           cant_obs_i=cant_obs_i[ind2] #elimino las cantidades observadas de los arboles vistos pocas veces
+    
+       a=squareform(pdist(mapa[:,0:Lact].T))  #calculo la matriz de distancias 2a2 de todas las posiciones de arboles observados
+       a[a==0]=np.amax(a) #reemplazo los ceros (de la diagonal generalmente) para que no interfiera en el calculo de minimos en las siguientes lineas
+       b=np.argmin(a,axis=0) #vector que contiene contiene el valor j en la entrada i, si el arbol j es el más cercano al arbol i
+       a=np.amin(a,axis=0) #vector que contiene la distancia minima entre los arboles i y j de la linea anterior
+       ind=np.where(a<self.dist_thr)[0] #indices donde la distancia entre dos arboles es muy chica
+       c=np.arange(Lact)  #contiene los indices de los arboles
+       if ind.size>0:  #si hay arboles muy cercanos los unifico
+           for i in range(len(ind)): #el arbol ind[i] tiene al arbol b[ind[i]] muy cercano
+               c[c==c[b[ind[i]]]]=c[ind[i]]  #le asigno al arbol b[ind[i]] (y a todos los cercanos a él) el indice del arbol ind[i]
+    
+       for i in range(Lact-1,-1,-1):
+           if len(c[c==i])==0:  #si el arbol i perdió su indice por ser cercano a uno de indice menor
+               c[c>=i]=c[c>=i]-1 #a todos los de indice mayor a i le resto 1... ya que el indice i ya no existe
+    
+       Lact=max(c)+1 #actualizo la cantidad de arboles observados luego del filtro
+       mapa_filtrado=np.zeros((2,self.L)) #contendrá la posición media ponderada de acuerdo a cant_obs_i entre todos los arboles unificados por estar cercanos 
+       cant_obs=np.zeros(self.L)  #reemplazará a cant_obs_i
+       for i in range(Lact):
+           cant_obs[i]=np.sum(cant_obs_i[c==i])
+           mapa_filtrado[:,i]=np.sum(mapa[:,c==i]*np.matlib.repmat(cant_obs_i[c==i],2,1),axis=1)/cant_obs[i] #calculo el centro de cada nuevo cluster
+       
+       self.Landmarks_actuales=Lact
+       self.cant_obs_i=cant_obs_i
+
+       return mapa_filtrado
+
+def tras_rot_z(x,z):
+    """
+     - Rota y traslada las observaciones de acuerdo a la pose actual
+     - Transforma del body frame al global frame
+
+    :math:`x_t=[ x_{t,x}, x_{t,y}, x_{t,\theta}]^T` 
+    :math:`z_i=\{z_{t,i}:i=1,\cdots,n_t\}` :math:`z_{t,i}=[ z_{t,i,d}, z_{t,i,\theta} ]^T`
+
+    """
+
+    x=x.reshape((3,1))
+    ct=np.cos(x[2]-np.pi/2.0)[0]
+    st=np.sin(x[2]-np.pi/2.0)[0]
+    R=np.array([[ct,st],[-st,ct]])
+    z[:,2:4]=np.matmul(z[:,2:4],R)+np.matlib.repmat(x[0:2].T,z.shape[0],1)
+    return z
 
 def Rota(theta):
     """
     Arma la matriz de rotación en 2D a partir de un ángulo en *radianes*.
-    Revisar $1
     """
     A=np.array([[np.cos(theta),np.sin(theta)],
         [-np.sin(theta),np.cos(theta)]])
@@ -400,6 +623,11 @@ def graficar_cambio(cambios_minimos,cambios_maximos,cambios_medios):
 
 
 if __name__=='__main__':
+    # lectura de datos
+    # inicializar variables
+    # iteracion 0
+    # iteraciones ICM 
+
 
 
     data=sio.loadmat('data_IJAC2018.mat')
@@ -408,9 +636,9 @@ if __name__=='__main__':
     u = np.array(data['velocities'])#2x1833
     del(data)
     
-    print(odometria.shape)
-    print(z.shape)
-    print(u.shape)
+    #print(odometria.shape)
+    #print(z.shape)
+    #print(u.shape)
 
     config=ConfigICM(z.shape[1])# Carga toda la configuración inicial
     ICM=ICM_method(config) # Crea el objeto de los minimizadores
@@ -436,7 +664,7 @@ if __name__=='__main__':
     x=np.zeros((3,config.Tf))  #guarda la pose del DDMR en los Tf periodos de muestreo
     
     #1) Iteracion inicial ICM
-    xt=copy(x0)#+0.0 #pose inicial. $1 Forma de copiar. 
+    xt=copy(x0) #pose inicial.  
     z=filtrar_z(zz[:,0],config)  #filtro la primer observacion [dist ang x y] x #obs
     zt=tras_rot_z(xt,z) #rota y traslada las observaciones de acuerdo a la pose actual
     y,cant_obs_i,c,Lact=actualizar_mapa(y,y,zt[:,2:4],0,config)
@@ -470,9 +698,22 @@ if __name__=='__main__':
     
     
     #2) Iteraciones ICM
+    """
+    Argumentos de entrada:
+     - 'config'
+     - Mediciones 'z'
+     - Mapa actual 'yy'
+     - Cantidad de landmarks vistos 'Lact'
+     - acciones de control 'u'
+     - Posiciones actuales 'x'
+    
+    Argumentos de salida:
+     - Mapa actualizado 'yy'
+     - Posiciones refinadas 'x' 
+    """
     for iteracionICM in range(config.N):
         print('iteración ICM : ',iteracionICM+1)
-        xt=copy(x0)#+0.0 #pose inicial
+        xt=copy(x0) #pose inicial
         y=np.zeros((2,config.L)) #guarda la posicion de los a lo sumo L arboles del entorno
         cant_obs_i=np.zeros(config.L)  #guarda la cantidad de veces que se observó el i-ésimo árbol
         z=filtrar_z(zz[:,0],config)  #filtro la primer observacion [dist ang x y] x #obs
@@ -507,7 +748,7 @@ if __name__=='__main__':
         cambios_minimos[iteracionICM]=cambio_minimo
         cambios_maximos[iteracionICM]=cambio_maximo
         cambios_medios[iteracionICM]=cambio_medio
-        mapa_viejo=copy(yy)#+0.0
+        mapa_viejo=copy(yy)
     
         graficar(x,yy,iteracionICM)#gráficos
 
