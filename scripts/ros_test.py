0d677e9466d8d1a71f156a9b0a495d2c2b3250a2
seba <sansoni.sebas@gmail.com>
2021-05-13 11:57:33 -0300

    no se hacer un stash, por lo que lo hago por partes

---
+++ scripts/ros_test.py
@@ -0,0 +1,23 @@
+import roslibpy
+
+
+def handler(msg):
+    print('Heard talking: ' + msg['data'])
+
+if __name__=='__main__':
+    client = roslibpy.Ros(host='localhost', port=9090)
+    client.run()
+    print('Is ROS connected?', client.is_connected)
+    #client.terminate()
+
+
+    listener = roslibpy.Topic(client, '/chatter', 'std_msgs/String')
+    listener.subscribe(handler)
+
+
+    
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        client.terminate()
