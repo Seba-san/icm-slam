6d37d9fba13a1a862df65c178d862110d02f6c80
seba <sansoni.sebas@gmail.com>
2021-05-13 10:19:09 -0300

    commit anterior a hacer el ros package

--- scripts/ICM_SLAM.py
+++ scripts/ICM_SLAM.py
@@ -279,6 +279,7 @@ class ICM_method():
     
         #BUCLE TEMPORAL
         for t in range(1,self.config.Tf):
+            # aca va el bucle ros.
             xtc=self.g(xt,u[:,t-1])  #actualizo cinemáticamente la pose
             z=filtrar_z(self.mediciones[:,t],self.config)  #filtro observaciones no informativas del tiempo t: [dist ang x y] x #obs
             if z.shape[0]==0:
@@ -295,11 +296,6 @@ class ICM_method():
 
         yy=self.mapa_obj.filtrar(y)
         yy=yy[:,:self.mapa_obj.landmarks_actuales]
-        #print(mapa.Landmarks_actuales)
-        #print(mapa.cant_obs_i)
-        #print(yy)
-        
-      
         mapa_inicial=copy(yy)
 
         #graficar(x,yy,iteracionICM)#gráficos
@@ -368,28 +364,71 @@ class ICM_method():
         mapa_refinado=copy(yy)
 
         #graficar(x,yy,iteracionICM)#gráficos
-        return mapa_refinado,x 
+        return mapa_refinado,x
 
-    def g(self,xt,ut):
-        print('Se necesita definir la cinematica del vehículo')
-        #msg=" en la funcion externa crear una funcion llamada g.\
-        #        La salida debe estar la pose del vehiculo"
-        #print(msg)
-        #logging.error("lal")
-        #logging.error('Se necesita definir la cinematica del vehículo')
-        sys.exit()
-        return np.zeros((3,1))
+    def g(self, xt,ut):
+        """
+        Modelo de la odometría
+        ========================
+        Actualización de la pose a partir de las señales de control usando solo la
+        cinemática del vehículo. Como referencia revisar definicion en el paper,
+        página 8/15. 
+        Entradas:
+         - :math:`x_t=[ x_{t,x}, x_{t,y}, x_{t,\theta}]^T` 
+         - :math:`u_t=[ v_t, \omega_t ]^T`
+         - config: Objeto que contiene todas las configuraciones
+    
+        Salida
+         - :math:`x_{t+1}=[ x_{t+1,x}, x_{t+1,y}, x_{t+1,\theta}]^T`
+    
+         A futuro poner la ecuación en este lugar.
+        """
+    
+        xt=xt.reshape((3,1))
+        ut=ut.reshape((2,1))
+        S=np.array([[(np.cos(xt[2]))[0],0.0],
+            [np.sin(xt[2])[0],0.0],
+            [0.0,1.0]])
+        gg=xt+self.config.deltat*np.matmul(S,ut).reshape((3,1))
+        return gg
+    
+    def h(self, xt,zt):
+        """
+        Función de potencial energético debido a las observaciones y al mapa. 
+    
+        Modelo de las observaciones
+        =============================
+        Página 8/15 del paper.
+        Modelo de las observaciones para el Laser 2D
+        Entradas:
+         - :math:`x_t=[ x_{t,x}, x_{t,y}, x_{t,\theta}]^T` 
+         - :math:`z_i=\{z_{t,i}:i=1,\cdots,n_t\}` :math:`z_{t,i}=[ z_{t,i,d}, z_{t,i,\theta} ]^T`
+         - config: Objeto que contiene todas las configuraciones
+    
+        Salida:
+         -  Potencial.
+    
+         Poner la forma de la función que esta en el paper.
+        """
+    
+        y=self.mapa_visto
+        alfa=zt[:,1]+xt[2]-np.pi/2.0
+        zc=zt[:,0]*np.cos(alfa)
+        zs=zt[:,0]*np.sin(alfa)
+        # Resta la posicion de cada punto al mapa "visto". (y no es todo el mapa,
+        # solo la parte matcheada en "actualizar mapa")
+        distancias=np.concatenate((xt[0]+zc,xt[1]+zs)).reshape((len(alfa),2),order='F')-y
+        # Calcula la norma :math:`hh^TQhh`
+        aux=np.matmul(distancias,self.config.Q)
+        potencial=np.sum(aux*distancias)
+        return potencial
 
-    def h(self,xt,zt):
-        print('Se necesita definir la funcion de medicion')
-        sys.exit()
-        return 0
 
 class ConfigICM:
     """
     Núcleo de todos los parámetros de configuración y constantes. 
     """
-    def __init__(self,Tf,configFile='config_default.yaml',D={}):
+    def __init__(self,configFile='config_default.yaml',D={}):
         if not D:
             arch=open(configFile, 'r')
             Data = yaml.load(arch,Loader=yaml.FullLoader)
@@ -399,7 +438,6 @@ class ConfigICM:
         # parámetros por default
         self.N=D['N']  #cantidad de iteraciones de ICM
         self.deltat=D['deltat']  #periodo de muestreo
-        self.Tf=Tf  #cantidad total de periodos de muestreo
         self.L=D['L']  #cota superior de la cantidad de objetos (max landmarks)
         
         self.Q=np.eye(2)#matriz de covarianza de las observaciones
@@ -418,7 +456,11 @@ class ConfigICM:
         self.rango_laser_max=D['rango_laser_max']  #alcance máximo del laser
         self.radio=D['radio'] #radio promedio de los árboles
 
+        self.topic=D['topic']
+        self.file=D['file']
 
+    def set_Tf(self,Tf):
+        self.Tf=Tf  #cantidad total de periodos de muestreo
 
 class Mapa:
     """
--- scripts/external_options.py
+++ scripts/external_options.py
@@ -7,78 +7,33 @@ from ICM_SLAM import *
 
 class ICM_external(ICM_method):
     """
-    clase modificable por el usuario. Puede sobreescribir las funciones fun_x y
-    fun_xn con el objetivo de determinar su propio funcional.
+    clase modificable por el usuario. Puede sobreescribir las funciones fun_x
+    fun_xn, g y h con el objetivo de determinar su propia configuracion.
     """
     def __init__(self,config):
         ICM_method.__init__(self,config)
 
 
-    def g(self, xt,ut):
-        """
-        Modelo de la odometria
-        ========================
-        Actualización de la pose a partir de las señales de control usando solo la
-        cinemática del vehículo. Como referencia revisar definicion en el paper,
-        página 8/15. 
-        Entradas:
-         - :math:`x_t=[ x_{t,x}, x_{t,y}, x_{t,\theta}]^T` 
-         - :math:`u_t=[ v_t, \omega_t ]^T`
-         - config: Objeto que contiene todas las configuraciones
-    
-        Salida
-         - :math:`x_{t+1}=[ x_{t+1,x}, x_{t+1,y}, x_{t+1,\theta}]^T`
-    
-         A futuro poner la ecuación en este lugar.
-        """
-    
-        xt=xt.reshape((3,1))
-        ut=ut.reshape((2,1))
-        S=np.array([[(np.cos(xt[2]))[0],0.0],
-            [np.sin(xt[2])[0],0.0],
-            [0.0,1.0]])
-        gg=xt+self.config.deltat*np.matmul(S,ut).reshape((3,1))
-        return gg
-    
-    def h(self, xt,zt):
-        """
-        Función de potencial energético debido a las observaciones y al mapa. 
-    
-        Modelo de las observaciones
-        =============================
-        Página 8/15 del paper.
-        Modelo de las observaciones para el Laser 2D
-        Entradas:
-         - :math:`x_t=[ x_{t,x}, x_{t,y}, x_{t,\theta}]^T` 
-         - :math:`z_i=\{z_{t,i}:i=1,\cdots,n_t\}` :math:`z_{t,i}=[ z_{t,i,d}, z_{t,i,\theta} ]^T`
-         - config: Objeto que contiene todas las configuraciones
-    
-        Salida:
-         -  Potencial.
-    
-         Poner la forma de la función que esta en el paper.
-        """
-    
-        y=self.mapa_visto
-        alfa=zt[:,1]+xt[2]-np.pi/2.0
-        zc=zt[:,0]*np.cos(alfa)
-        zs=zt[:,0]*np.sin(alfa)
-        # Resta la posicion de cada punto al mapa "visto". (y no es todo el mapa,
-        # solo la parte matcheada en "actualizar mapa")
-        distancias=np.concatenate((xt[0]+zc,xt[1]+zs)).reshape((len(alfa),2),order='F')-y
-        # Calcula la norma :math:`hh^TQhh`
-        aux=np.matmul(distancias,self.config.Q)
-        potencial=np.sum(aux*distancias)
-        return potencial
+    def g_(self,xt,ut):
+        print('Se necesita definir la cinematica del vehículo')
+        sys.exit()
+        return np.zeros((3,1))
 
-    def fun_xxn(self,x):
-        """
-        Esta función debe devolver un float para el cual su valor deve ser
-        pequeño para posiciones de x más probables y un valor grande para
-        valores improbables
+    def h_(self,xt,zt):
+        print('Se necesita definir la funcion de medicion')
+        sys.exit()
+        return 0
 
-        """
-        a=1
+    def fun_x_(self):
+        print('Se necesita definir el argumento a minimizar para las
+                iteraciones online')
+        sys.exit()
+        return 0
+
+    def fun_xn_(self):
+        print('Se necesita definir el argumento a minimizar para las
+                iteraciones offline')
+        sys.exit()
         return 0
 
 if __name__=='__main__':
@@ -90,20 +45,20 @@ if __name__=='__main__':
      - iteraciones ICM 
     """
 
+    # Inicializacion de variables y objetos
+    config=ConfigICM()# Carga toda la configuración inicial
     # Lectura de datos
-    data=sio.loadmat('data_IJAC2018.mat')
+    data=sio.loadmat(config.file)
     odometria = np.array(data['odometry'])#3x1833
     z = np.array(data['observations']) # 181x1833
     u = np.array(data['velocities'])#2x1833
     del(data)
     
-    
-    # Inicializacion de variables y objetos
-    config=ConfigICM(z.shape[1])# Carga toda la configuración inicial
     #Filtro de observaciones
     zz=np.minimum(z+config.radio,z*0.0+config.rango_laser_max)
 
     #ICM=ICM_method(config) # Crea el objeto de los minimizadores
+    config.set_Tf(z.shape(1)) 
     ICM=ICM_external(config) # Crea el objeto de los minimizadores
     mapa_obj=Mapa(config)
     ICM.load_data(mapa_obj,zz,u,odometria)
